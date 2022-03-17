aa5df1a12e46ebc3f7d4d8fe0367cd9fa32cfccc
twopunchman <ray.wang.pro@gmail.com>
2022-03-17 16:55:34 -0400

    added basic functions to js script +-*/

---
+++ script.js
@@ -0,0 +1,21 @@
+/*  script for calculator functions
+	The odin project - calculator
+*/
+
+// 1. basic functions
+const add = function(a, b) {
+	return a + b;
+}
+
+const subtract = function(a, b) {
+	return a - b;
+}
+
+const multiply = function(a, b) {
+	return a * b;
+}
+
+const divide = function(a, b) {
+	return a / b;
+}
+

a6f36a3e4f49ec2fab626afb0e5cc38a0d988433
twopunchman <ray.wang.pro@gmail.com>
2022-03-18 16:56:52 -0400

    working on digit, operator, clear buttons logic

--- script.js
+++ script.js
@@ -2,7 +2,55 @@
 	The odin project - calculator
 */
 
-// 1. basic functions
+// variables
+let first_num;
+let second_num;
+let operator;
+
+// get selectors
+const output = document.querySelector('.output-window');
+const digit_btns = document.querySelectorAll('.digit');
+const operator_btns = document.querySelectorAll('.operator');
+const equal_btn = document.querySelector('.equals');
+const clear_btn = document.querySelector('.clear');
+
+// set output window to 0
+output.textContent = 0;
+
+digit_btns.forEach(btn => 
+	btn.addEventListener('click', function() {
+		if (!first_num) {
+			first_num = parseInt(btn.value);
+		} else {
+			second_num = parseInt(btn.value);
+		}
+		output.textContent = btn.value;
+	})
+);
+
+operator_btns.forEach(btn => btn.addEventListener('click', function() {
+	if (!operator) {
+		operator = btn.value;
+	}
+}));
+
+equal_btn.addEventListener('click', function() {
+	output.textContent = operate(operator, first_num, second_num);
+	operator = null;
+});
+
+clear_btn.addEventListener('click', function() {
+	output.textContent = 0;
+	first_num = null;
+	second_num = null;
+	operator = null;
+});
+
+function getNum(btn) {
+	return btn.value;
+}
+
+//  basic functions
 const add = function(a, b) {
 	return a + b;
 }
@@ -19,7 +67,7 @@ const divide = function(a, b) {
 	return a / b;
 }
 
-// 2. takes operator and calls it for a,b
+//  takes operator and calls it for a,b
 const operate = function(operator, a, b) {
 
 	let result;

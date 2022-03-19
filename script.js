2692bf4f6958b706335b3611452e9bdd2513c766
twopunchman <ray.wang.pro@gmail.com>
2022-03-19 11:01:12 -0400

    modified digit, operator, equal buttons logic to reflect a real life digital calculator behavior

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 let first_num;
 let second_num;
 let operator;
+let is_second_num_exist = false;
+let is_operator_pressed = false;
+let result;
 
 // get selectors
 const output = document.querySelector('.output-window');
@@ -19,24 +22,59 @@ output.textContent = 0;
 
 digit_btns.forEach(btn => 
 	btn.addEventListener('click', function() {
-		if (!first_num) {
-			first_num = parseInt(btn.value);
-		} else {
-			second_num = parseInt(btn.value);
-		}
-		output.textContent = btn.value;
+		handle_number_input(btn.value);
 	})
 );
 
-operator_btns.forEach(btn => btn.addEventListener('click', function() {
-	if (!operator) {
-		operator = btn.value;
+function handle_number_input(button_val) {
+	if (is_operator_pressed) {
+		output.textContent = '';
+		is_operator_pressed = false;
+		is_second_num_exist = true;
 	}
+
+	if (output.textContent == '0') {
+		output.textContent = button_val;
+	} else {
+		output.textContent += button_val;
+	}
+
+	result = output.textContent;
+	console.log(result);
+}
+function handle_window(button_val, operator) {
+	if (!operator) {
+		handle_number_input(button_val);
+	} else {
+
+	}
+}
+
+operator_btns.forEach(btn => btn.addEventListener('click', function() {
+	if (!first_num) {
+		first_num = parseInt(output.textContent);
+	} 
+	if (is_second_num_exist) {
+		second_num = parseInt(output.textContent);
+		first_num = result = operate(operator, first_num, second_num);
+		output.textContent = result;
+	}
+	result = output.textContent;
+	operator = btn.value;
+	is_operator_pressed = true;
+	console.log(`${first_num} ${operator}`)
+
 }));
 
 equal_btn.addEventListener('click', function() {
-	output.textContent = operate(operator, first_num, second_num);
-	operator = null;
+	if (is_second_num_exist) {
+		second_num = parseInt(output.textContent);
+		first_num = result = operate(operator, first_num, second_num);
+		output.textContent = result;
+		second_num = null;
+		operator = null;
+		is_second_num_exist = false;
+	} 
 });
 
 clear_btn.addEventListener('click', function() {
@@ -44,6 +82,7 @@ clear_btn.addEventListener('click', function() {
 	first_num = null;
 	second_num = null;
 	operator = null;
+	result = null;
 });
 
 function getNum(btn) {

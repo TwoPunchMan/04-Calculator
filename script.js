87d2b5ba54fa2a6fa0f7a9cb6026fd0e2f849f4e
twopunchman <ray.wang.pro@gmail.com>
2022-03-19 17:11:56 -0400

    changed logic to handle floats and added back, decimal buttons, changed html buttons, css placement of buttons

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 let first_num;
 let second_num;
 let operator;
+let is_decimal_exist = false;
 let is_first_num_exist = false;
 let is_second_num_exist = false;
 let is_operator_pressed = false;
@@ -14,12 +15,38 @@ let result;
 // get selectors
 const output = document.querySelector('.output-window');
 const digit_btns = document.querySelectorAll('.digit');
+const decimal_btn = document.querySelector('.decimal');
+const back_btn = document.querySelector('.back');
 const operator_btns = document.querySelectorAll('.operator');
 const equal_btn = document.querySelector('.equals');
 const clear_btn = document.querySelector('.clear');
 
 // set output window to 0
 output.textContent = 0;
+
+// add decimal if not exist to number
+decimal_btn.addEventListener('click', function() {
+	if (!is_decimal_exist) {
+		output.textContent += '.';
+		is_decimal_exist = true;
+	}
+});
+
+// undo last key input
+back_btn.addEventListener('click', function() {
+	if (output.textContent) {
+		// delete last key input and update screen
+		let parser = output.textContent.charAt(output.textContent.length - 1);
+		output.textContent = output.textContent.slice(0, output.textContent.length - 1);
+		
+		// if decimal deleted
+		if (parser == '.') is_decimal_exist = false;
+	}
+	
+	if (output.textContent == '') {
+		output.textContent = '0';
+	}
+});
 
 digit_btns.forEach(btn => 
 	btn.addEventListener('click', function() {
@@ -42,20 +69,22 @@ digit_btns.forEach(btn =>
 		} else {
 			output.textContent += btn.value;
 		}
+
 		result = output.textContent;
+		console.log(result);
 	})
 );
 
 operator_btns.forEach(btn => btn.addEventListener('click', function() {
 	// if no first num store the value as soon as operator pressed
 	if (!first_num) {
-		first_num = parseInt(result);
+		first_num = parseFloat(output.textContent);
 		is_first_num_exist = true;
 	} 
 
 	// if there is a second num and this function for chaining operators
 	if (is_second_num_exist) {
-		second_num = parseInt(result);
+		second_num = parseFloat(output.textContent);
 		first_num = result = operate(operator, first_num, second_num);
 		output.textContent = result;
 	}
@@ -68,15 +97,23 @@ operator_btns.forEach(btn => btn.addEventListener('click', function() {
 // evaluate number and operators
 equal_btn.addEventListener('click', function() {
 	if (is_second_num_exist) {
-		second_num = parseInt(output.textContent);
-		result = operate(operator, first_num, second_num);
+		// only chcuk norris can divide by 0
+		second_num = parseFloat(output.textContent);
+		if (operator == '/' && second_num == '0') {
+			result = "Muda Muda Muda!";
+		} else {
+			result = operate(operator, first_num, second_num);
+		}
+
 		output.textContent = result;
 		first_num = null;
 		second_num = null;
 		operator = null;
 		is_first_num_exist = false;
 		is_second_num_exist = false;
-	} 
+	}
+
+	
 });
 
 // clear variables cache totally
